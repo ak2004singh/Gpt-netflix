fefa05c9d71d03509c11f3370390742901792e44
ak2004-star <officialadityasingh21@gmail.com>
2025-07-17 00:12:41 +0530

    all setup done

---
+++ src/Component/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom';
+const App = () => {
+  return (
+    <div></div>
+  )
+}
+const routes = createBrowserRouter([]);
+export default App
